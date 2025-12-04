bd3326014fecad53fc44074cc51199043180ed51
Riad-chowdhury <arjackson824119@gmail.com>
2025-12-05 02:59:41 +0600

    users delete complite

--- index.js
+++ index.js
@@ -129,14 +129,20 @@ async function run() {
       });
       res.send(result);
     });
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-    app.get("/users", async (req, res) => {
-      const result = await usersCollection.find().toArray();
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
     // ////
